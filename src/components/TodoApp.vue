be5880b75c637c4f22a556594f896c4756c0d4be
gunwoongPark <bung1438@gmail.com>
2020-09-12 13:10:47 +0900

    no message

--- src/components/TodoApp.vue
+++ src/components/TodoApp.vue
@@ -8,6 +8,7 @@
 <script>
 import lowdb from "lowdb";
 import LocalStorage from "lowdb/adapters/LocalStorage";
+import cryptoRandomString from "crypto-random-string";
 import TodoCreator from "./TodoCreator";
 import TodoItem from "./TodoItem";
 
@@ -23,18 +24,31 @@ export default {
   },
   created() {
     this.initDB();
+    console.log(typeof this.db);
   },
   methods: {
     initDB() {
       const adapter = new LocalStorage("todo-app");
       this.db = lowdb(adapter);
 
+      console.log(this.db);
       // Local DB 초기화
       this.db
         .defaults({
-          todos: [],
+          todos: [], // Collections
         })
         .write();
+    },
+    createTodo(newTitle) {
+      const newTodo = {
+        id: cryptoRandomString({ length: 10 }),
+        title: newTitle,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        done: false,
+      };
+
+      this.db.get("todos").push(newTodo).write();
     },
   },
 };

fa898df212de06d8dcd61fedf3fd7b2c9e14d6c6
gunwoongPark <bung1438@gmail.com>
2020-09-15 15:34:21 +0900

    router 설정

--- router/index.js
+++ router/index.js
@@ -1,10 +1,26 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
+import Home from '../src/views/Home.vue'
+import About from '../src/views/About.vue'
+import TodoApp from '../src/views/TodoApp'
+
 Vue.use(VueRouter)
 
 const routes = [
     // config
+    {
+        path: '/',
+        component: Home
+    },
+    {
+        path: '/about',
+        component: About
+    },
+    {
+        path: '/todos',
+        component: TodoApp
+    }
 ]
 
 export default new VueRouter({
---
+++ src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <section class="home">
+    <div>
+      <h1>gunwoongPark's Home page!</h1>
+      <h2 @click="toAbout">About</h2>
+      <h2 @click="toTodoApp">Todo App</h2>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  methods: {
+    toAbout() {
+      this.$router.push("/about");
+    },
+    toTodoApp() {
+      this.$router.push("/todos");
+    },
+  },
+};
+</script>

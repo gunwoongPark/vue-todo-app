193746b5b00498b5323d718f8d096ee50b5b0060
gunwoongPark <bung1438@gmail.com>
2020-09-16 22:50:17 +0900

    vuex 설정

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,13 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+import todoApp from "./todoApp"
+
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
+  strict: process.env.NODE_ENV !== 'production',
   modules: {
+    todoApp
   }
 })
---
+++ src/store/todoApp.js
@@ -0,0 +1,16 @@
+export default {
+    namespaced: true,
+    // Data
+    // 참조 관계가 발생하지 않도록 함수형으로 작성
+    state: () => ({
+
+    }),
+    // Computed
+    getters: {},
+    // Methods
+    // 실제 값을 변경할 때(비동기X)
+    mutations: {},
+    // Methods
+    // 일반 로직(비동기O)
+    actions: {}
+}
